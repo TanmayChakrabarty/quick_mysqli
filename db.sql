5f8b06e18978d7580ec18fecc0bf2644e2267f67
Tanmay Chakrabarty <cse.tanmay@gmail.com>
2019-04-17 02:36:52 +0600

    working

---
+++ db.sql
@@ -0,0 +1,32 @@
+/*
+SQLyog Ultimate v11.33 (64 bit)
+MySQL - 5.7.19 : Database - quick_mysql_test_db
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `_users` */
+
+DROP TABLE IF EXISTS `_users`;
+
+CREATE TABLE `_users` (
+  `pk_user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`pk_user_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `_users` */
+
+insert  into `_users`(`pk_user_id`,`firstname`) values (1,'Raju'),(2,'Sohel'),(3,'Jaman'),(4,'Jamil'),(5,'Kamal'),(6,'Tamal'),(7,'Rajib'),(8,'Sanju'),(9,'Sajib'),(10,'Rakib');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
--- index.php
+++ index.php
@@ -6,8 +6,13 @@ $db->connect();
 $db->select();
 
 //using AS_RAW in get_results()
-$data = $db->get_results( "SELECT * FROM _users", null, AS_RAW);
+// $data = $db->get_results( "SELECT * FROM _users", null, AS_RAW);
+// echo '<pre>';
+// while ( $row = @$data->fetch_object() ){
+//     print_r($row);
+// }
+
+//insert using the default query function
+$ret = $db->query("INSERT INTO _users (pk_user_id, firstname) VALUES(NULL,'Rakib')");
 echo '<pre>';
-while ( $row = @$data->fetch_object() ){
-    print_r($row);
-} 
+print_r($ret);
