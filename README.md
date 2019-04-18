212a92cd7e7410a5b513214ea00d62f1dc5231c6
Tanmay Chakrabarty <cse.tanmay@gmail.com>
2019-04-19 02:40:38 +0600

    testing insert

--- README.md
+++ README.md
@@ -11,13 +11,12 @@ result_format can be AS_RAW, OBJECT, ARRAY_N, ARRAY_A
  - you should use this format when you don't need to run multiple queries, for example a query that collects the data for a csv report.
  - this is the fastest, no extra loops :)
 
- #### example 
+ #### example
  ```
 $data = $db->get_results( "SELECT * FROM _users", null, AS_RAW);
 while ( $row = @$data->fetch_object() ){
     print_r($row);
-} 
+}
  ```
 
 though I am fetching as object, you can fetch as any as you want such as fetch_assoc() for associative array or fetch_array() for numeric array indexed
-
--- index.php
+++ index.php
@@ -1,18 +1,26 @@
-<?php 
+<?php
 include( 'class.quick_mysqli.php' );
 
 $db = new quick_mysqli( 'root', '', 'quick_mysql_test_db', 'localhost' );
 $db->connect();
 $db->select();
 
+echo '<pre>';
+
 //using AS_RAW in get_results()
-// $data = $db->get_results( "SELECT * FROM _users", null, AS_RAW);
-// echo '<pre>';
-// while ( $row = @$data->fetch_object() ){
-//     print_r($row);
-// }
+$data = $db->get_results( "SELECT  FROM _users", null, AS_RAW);
+echo '<pre>';
+var_dump($data);
+while ( $row = @$data->fetch_object() ){
+    print_r($row);
+}
+exit();
 
 //insert using the default query function
-$ret = $db->query("INSERT INTO _users (pk_user_id, firstname) VALUES(NULL,'Rakib'),(NULL,'Sakib')");
-echo '<pre>';
+//$ret = $db->query("INSERT INTO _users (pk_user_id, firstname) VALUES(NULL,'Rakib'),(NULL,'Sakib')");
+//print_r($ret);
+
+//updating multiple rows
+$ret = $db->query("UPDATE _users SET firstame='ZZZZ' WHERE pk_user_id='1000'");
 print_r($ret);
+print_r($db);
